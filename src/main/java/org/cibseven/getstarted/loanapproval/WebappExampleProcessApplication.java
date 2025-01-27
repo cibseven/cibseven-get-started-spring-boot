camunda/camunda-get-started-spring-boot

Step-3: deploy and invoke BPMN process

src/main/java/org/camunda/bpm/getstarted/loanapproval/WebappExampleProcessApplication.java
@@ -1,11 +1,26 @@
 package org.camunda.bpm.getstarted.loanapproval;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class WebappExampleProcessApplication {
+	
+	@Autowired
+	private RuntimeService runtimeService;
+	
 	public static void main(String... args) {
 		SpringApplication.run(WebappExampleProcessApplication.class, args);
+	}
+	
+	@EventListener
+	private void processPostDeploy(PostDeployEvent event) {
+		runtimeService.startProcessInstanceByKey("loanApproval");
 	}
 }

cibseven/cibseven-get-started-spring-boot

src/main/java/org/cibseven/getstarted/loanapproval/WebappExampleProcessApplication.java
@@ -16,14 +16,28 @@
  */
 package org.cibseven.getstarted.loanapproval;
 
+import org.cibseven.bpm.engine.RuntimeService;
+import org.cibseven.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.cibseven.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class WebappExampleProcessApplication {
+
+  @Autowired
+  private RuntimeService runtimeService;
 
   public static void main(String... args) {
     SpringApplication.run(WebappExampleProcessApplication.class, args);
+  }
+
+  @EventListener
+  public void processPostDeploy(PostDeployEvent event) {
+    runtimeService.startProcessInstanceByKey("loanApproval");
   }
 
 }