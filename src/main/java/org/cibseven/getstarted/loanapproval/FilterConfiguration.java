9dbfecbfc858ec9171077d54b360013d31f15e79
Fernando Mambrilla Garcia <FernandoM@cib.de>
2025-06-23 12:57:24 +0200

    feat(spring-boot-get-started): adapt auth filter to rest engine path

--- src/main/java/org/cibseven/getstarted/loanapproval/FilterConfiguration.java
+++ src/main/java/org/cibseven/getstarted/loanapproval/FilterConfiguration.java
@@ -1,7 +1,7 @@
 package org.cibseven.getstarted.loanapproval;
 
 import org.cibseven.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
-
+import org.springframework.boot.autoconfigure.web.servlet.JerseyApplicationPath;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,43 +12,54 @@ import java.util.Arrays;
 public class FilterConfiguration {
 
     @Bean
-    public FilterRegistrationBean<ProcessEngineAuthenticationFilter> AuthenticationFilter() {
-    	// Composite Authentication Filter with Jwt Token and Http Basic
-        FilterRegistrationBean<ProcessEngineAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new ProcessEngineAuthenticationFilter());
-
-        // Apply to all URLs under engine-rest except /engine-rest/identity/verify
-        registrationBean.setUrlPatterns(Arrays.asList(
-            "/engine-rest/process-definition/*",
-            "/engine-rest/process-instance/*",
-            "/engine-rest/history/*",
-            "/engine-rest/execution/*",
-            "/engine-rest/batch/*",
-            "/engine-rest/decision-definition/*",
-            "/engine-rest/deployment/*",
-            "/engine-rest/filter/*",
-            "/engine-rest/incident/*",
-            "/engine-rest/job-definition/*",
-            "/engine-rest/job/*",
-            "/engine-rest/telemetry/*",
-            "/engine-rest/metrics/*",
-            "/engine-rest/authorization/*",
-            "/engine-rest/group/*",
-            "/engine-rest/user/*",
-            "/engine-rest/message/*",
-            "/engine-rest/event-subscription/*",
-            "/engine-rest/variable-instance/*",
-            "/engine-rest/task/*"
-        ));
+    // Composite Authentication Filter with Jwt Token and Http Basic
+    public FilterRegistrationBean<ProcessEngineAuthenticationFilter> AuthenticationFilter(JerseyApplicationPath applicationPath) {
+    	FilterRegistrationBean<ProcessEngineAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setName("cibseven-composite-auth");
-        // Enable async support
+    	registrationBean.setFilter(new ProcessEngineAuthenticationFilter());
+        registrationBean.setOrder(10);// Order of execution if multiple filters
+
+        String restApiPathPattern = applicationPath.getPath();
+        
+        // Apply to all URLs under engine-rest except /engine-rest/identity/verify
+    	String[] urlPatterns = Arrays.asList(
+            "/process-definition/*",
+            "/process-instance/*",
+            "/history/*",
+            "/execution/*",
+            "/batch/*",
+            "/decision-definition/*",
+            "/deployment/*",
+            "/filter/*",
+            "/incident/*",
+            "/job-definition/*",
+            "/job/*",
+            "/telemetry/*",
+            "/metrics/*",
+            "/authorization/*",
+            "/group/*",
+            "/user/*",
+            "/message/*",
+            "/event-subscription/*",
+            "/variable-instance/*",
+            "/task/*",
+            "/engine/*",
+            "/identity/groups"
+    	).stream().map(pattern -> addUrl(restApiPathPattern, pattern)).toArray(String[]::new);
+
+    	// Enable async support
         registrationBean.setAsyncSupported(true);
         // Init parameters
         registrationBean.addInitParameter(
             "authentication-provider",
             org.cibseven.bpm.engine.rest.security.auth.impl.CompositeAuthenticationProvider.class.getName()
         );
-        registrationBean.setOrder(10);// Order of execution if multiple filters
+
+        registrationBean.addUrlPatterns(urlPatterns);
         return registrationBean;
+    }
+    
+    private String addUrl(String base, String extend) {
+  	  return (base + extend).replaceFirst("^(\\/+|([^/]))", "/$2");
     }
 }
