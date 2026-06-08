a45ebc3ffc4f97b950e85aa55d2663d587d67172
Fernando Mambrilla Garcia <fernandom@cib.de>
2026-06-08 16:53:49 +0200

    chore(pom): update dependencies and versions to CIB seven 2.2.0 and Spring Boot 4.0.6

---
+++ src/main/java/org/cibseven/getstarted/loanapproval/FilterConfiguration.java
@@ -0,0 +1,40 @@
+package org.cibseven.getstarted.loanapproval;
+
+import org.cibseven.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
+import org.springframework.boot.jersey.autoconfigure.JerseyApplicationPath;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfiguration {
+
+    @Bean
+    // Composite Authentication Filter with Jwt Token and Http Basic
+    public FilterRegistrationBean<ProcessEngineAuthenticationFilter> AuthenticationFilter(JerseyApplicationPath applicationPath) {
+    	FilterRegistrationBean<ProcessEngineAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setName("cibseven-composite-auth");
+    	registrationBean.setFilter(new ProcessEngineAuthenticationFilter());
+        registrationBean.setOrder(10);// Order of execution if multiple filters
+
+        String restApiPathPattern = applicationPath.getPath();
+        
+        // Apply to all URLs - whitelist logic in the filter handlers exclusions
+        String[] urlPatterns = new String[] { addUrl(restApiPathPattern, "/*") };
+
+    	// Enable async support
+        registrationBean.setAsyncSupported(true);
+        // Init parameters
+        registrationBean.addInitParameter(
+            "authentication-provider",
+            org.cibseven.bpm.engine.rest.security.auth.impl.CompositeAuthenticationProvider.class.getName()
+        );
+
+        registrationBean.addUrlPatterns(urlPatterns);
+        return registrationBean;
+    }
+    
+    private String addUrl(String base, String extend) {
+  	  return (base + extend).replaceFirst("^(\\/+|([^/]))", "/$2");
+    }
+}
