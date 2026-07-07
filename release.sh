05b46cf2e40e232588def75be47b7505e71a379b
Agustin-Cib <agustincasebonne9@gmail.com>
2026-07-07 11:21:26 +0100

    chore: moving to java 17

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 VERSION=${VERSION:-$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)}
-JAVA=${JAVA:-21}
+JAVA=${JAVA:-17}
 
 IMAGE=cibseven/cibseven-mcp-restapi
 
